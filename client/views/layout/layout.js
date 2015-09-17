55c5422ff470a4cb99d08e775c523c1d0c79c089
Sean Niesen <seanniesen@gmail.com>
2015-09-16 22:51:38 -0700

    getting navigation animations to work

--- client/views/layout/layout.js
+++ client/views/layout/layout.js
@@ -51,9 +51,8 @@ Template.layout.events({
 		}
 	},
 	'click .menuItem1': function(e, template) {
-		$(".homeLink").append("<div class='row homeLinkRow'><div class='homeMenuItem'><p class='menuText1'>Home</p></div></div>");
-		$(".homeMenuItem").animate({right: "+=210"}, 1000);
-		if (!$(".menuItem1").hasClass("arf")) {
+		if (!$(".menuItem1").hasClass("arf") && ($(".menuItem1").height() != 40)) {
+			$(".homeMenuItem").animate({right: "+=210"}, 1000);
 			$(".headSection").animate({height: "60px", marginBottom: "20px"}, 1000);
 			$(".headSection > canvas").animate({height: "60px"}, 1000);
 			$(".menuItem1").animate({right: "-=400"}, 1000).delay(1000).addClass("arf");
@@ -65,11 +64,30 @@ Template.layout.events({
 			$(".menuText1, .menuText2, .menuText3").animate({color: "black"}, 500);
 			//$(".mainContainer").append(portfolioHtml);
 			Session.set('portfolio', true);
+		} else if ($(".menuItem1").height() === 40) {
+			$(".menuItem1").animate({top: "250px", width: "+=200", height: "+=40", fontSize: "1em", backgroundColor: "#d8ff00", right: "-=400"}, 1000);
+			$(".menuItem2").animate({width: "-=200"});
+			$(".menuItem2").animate({top: "90px", height: "-=40", fontSize: ".5em", right: "+=400px", backgroundColor: "#00ff7f"}, 500);
+			$(".menuItem1").addClass("arf")
+			Session.set('portfolio', true);
+		}
+	},
+	'click .menuItem2': function(e, template) {
+		if (!$(".menuItem2").hasClass("arf")) {
+			$(".homeMenuItem").animate({right: "+=210"}, 1000);
+			$(".headSection").animate({height: "60px", marginBottom: "20px"}, 1000);
+			$(".headSection > canvas").animate({height: "60px"}, 1000);
+			$(".menuItem1").delay(300).animate({width: "-=200"}, 1000);
+			$(".menuItem2").animate({right: "-=400"}, 1000).delay(1000).addClass("arf");
+			$(".menuItem3").delay(600).animate({width: "-=200"}, 1000);
+
+			$(".menuItem1").animate({top: "90px", height: "-=40", fontSize: ".5em", backgroundColor: "#00ff7f"}, 500);
+			$(".menuItem3").animate({top: "140px", height: "-=40", fontSize: ".5em", backgroundColor: "#0099ff"}, 500);
+			$(".menuText1, .menuText2, .menuText3").animate({color: "black"}, 500);
 		}
 	},
 	'click .homeMenuItem': function(e, template) {
 		if ($(".menuItem1").hasClass("arf")) {
-			//$(".portfolioHtml").remove();
 			Session.set('portfolio', false);
 			$(".homeMenuItem").animate({right: "-=210"}, 300);
 			$(".headSection").animate({height: "400px"}, 1000);
@@ -79,6 +97,18 @@ Template.layout.events({
 			$(".menuItem3").delay(600).animate({width: "+=200"}, 1000);
 
 			$(".menuItem2").animate({top: "350px", height: "+=40", fontSize: "1em", backgroundColor: "#00ff94"}, 500);
+			$(".menuItem3").animate({top: "450px", height: "+=40", fontSize: "1em", backgroundColor: "#ff0000"}, 500);
+			$(".menuText1, .menuText2, .menuText3").animate({color: "#bbc107"}, 500);
+		} else if ($(".menuItem2").hasClass("arf")) {
+			Session.set('portfolio', false);
+			$(".homeMenuItem").animate({right: "-=210"}, 300);
+			$(".headSection").animate({height: "400px"}, 1000);
+			$(".headSection > canvas").animate({height: "400px"}, 1000);
+			$(".menuItem1").delay(300).animate({top: "250px", width: "+=200", height: "+=40", fontSize: "1em", backgroundColor: "#d8ff00"}, 1000);
+			$(".menuItem2").animate({right: "+=400"}, 1000).delay(1000);
+			$(".menuItem3").delay(600).animate({width: "+=200"}, 1000);
+
+			$(".menuItem2").animate({top: "350px", fontSize: "1em", backgroundColor: "#00ff94"}, 500).removeClass("arf");
 			$(".menuItem3").animate({top: "450px", height: "+=40", fontSize: "1em", backgroundColor: "#ff0000"}, 500);
 			$(".menuText1, .menuText2, .menuText3").animate({color: "#bbc107"}, 500);
 		}
