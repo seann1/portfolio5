2d4a480efe2e65a01745abea44b5eebf055b42a5
Sean Niesen <seanniesen@gmail.com>
2015-09-26 15:48:35 -0700

    looking for uihooks

--- client/views/about/about.js
+++ client/views/about/about.js
@@ -1,13 +1,27 @@
 Template.about.onRendered(function() {
 	Meteor.call("getRepos", function(error,result){
-    	Session.set("repos", result);
+    	Session.set("repos", _.flatten(result, true));
     });
+
+    function start() {
+    	setInterval(function(){
+    		$(".repoNames").empty().append(_.sample(Session.get("repos")).name);
+    	}, 500);
+    };
+
+    this.autorun(function() {
+		if (Session.get("repos") != undefined) {
+	    	console.log(Session.get("repos"));
+	    	Session.set("reposLoaded", true);
+	    	start();
+	    }
+	});
 });
 
 Template.about.helpers({
-	'allRepos': function() {
-		console.log(_.flatten(Session.get("repos"), true));
-		//return Session.get("repos");
-		return _.flatten(Session.get("repos"), true);
+	'reposLoaded': function() {
+		if (Session.get("reposLoaded") === true) {
+			return true
+		}
 	}
 });
